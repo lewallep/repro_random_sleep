4b80542edfdb3fd14a0195688646383e44994997
plewallen <plewallen@newrelic.com>
2016-01-29 15:23:32 -0800

    initial commit

---
+++ TestProject_02_IIS/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Threading;
+
+namespace TestProject_02_IIS.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            // methodOne();
+            // methodTwo();
+            // methodThree();
+            RandomLengthSleep();
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        // Soon to be deprecated
+        public void MethodOne()
+        {
+            Thread.Sleep(1000);
+        }
+
+        // Soon to be deprecated
+        public void MethodTwo()
+        {
+            Thread.Sleep(1000);
+        }
+
+        // Deprecated.
+        public void MethodThree()
+        {
+            Thread.Sleep(1000);
+        }
+
+        public void RandomLengthSleep()
+        {
+            int sleepTime = 100;
+
+            Random randomOne = new Random();
+
+            sleepTime = randomOne.Next(100, 120000);
+
+            Thread.Sleep(sleepTime);
+        }
+    }
+}
---
+++ TestProject_02_IIS/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TestProject_02_IIS.MvcApplication" Language="C#" %>
